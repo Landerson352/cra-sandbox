9b8a3f2fdd0ae4612251b89dcd230fc112d94202
Lincoln Anderson <landerson@352inc.com>
2019-02-04 18:49:37 -0500

    Add Roboto, demo login form and more hooks to UIkit.

--- src/App.js
+++ src/App.js
@@ -16,6 +16,35 @@ class App extends Component {
             </p>
           </div>
         </div>
+
+        <div className="uk-background-muted uk-padding-large">
+          <div className="uk-width-large uk-card uk-card-default uk-card-body uk-margin-auto-left uk-margin-auto-right">
+            <h2>Log in</h2>
+
+            <form className="uk-form-stacked">
+
+              <div className="uk-margin-bottom">
+                <label className="uk-form-label" htmlFor="login_username">Username</label>
+                <div className="uk-form-controls">
+                  <input className="uk-input" id="login_username" type="text" />
+                </div>
+              </div>
+
+              <div className="uk-margin-bottom">
+                <label className="uk-form-label" htmlFor="login_password">Password</label>
+                <div className="uk-form-controls">
+                  <input className="uk-input" id="login_password" type="password" />
+                </div>
+              </div>
+
+              <div className="uk-flex uk-flex-middle uk-flex-left uk-margin-large-top">
+                <button className="uk-button uk-button-primary" type="button">Log in</button>
+                <a href="#" className="uk-text-muted uk-margin-left">Need to create an account?</a>
+              </div>
+
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
