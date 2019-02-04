2d050f7b06788f34f76c4c76248150cd881b6b49
Lincoln Anderson <landerson@352inc.com>
2019-02-04 17:53:26 -0500

    Disable CSS modules.

--- craco.config.js
+++ craco.config.js
@@ -4,12 +4,6 @@ module.exports = {
   plugins: [
     {
       plugin: CracoLessPlugin,
-      options: {
-        cssLoaderOptions: {
-          modules: true,
-          localIdentName: "[local]_[hash:base64:5]"
-        }
-      }
     }
   ]
 };
--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
 
-import styles from './app.less';
-
 class App extends Component {
   render() {
     return (
-      <div className={styles.red}>
+      <div>
         <button className="uk-button uk-button-default uk-margin-small-right" type="button" uk-toggle="target: #modal-example">Open</button>
-
-        <a href="#modal-example" data-uk-toggle>Open</a>
 
         <div id="modal-example" data-uk-modal>
           <div className="uk-modal-dialog uk-modal-body">
@@ -16,7 +12,7 @@ class App extends Component {
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             <p className="uk-text-right">
               <button className="uk-button uk-button-default uk-modal-close" type="button">Cancel</button>
-              <button className="uk-button uk-button-primary" type="button">Save</button>
+              <button className="uk-button uk-button-primary uk-margin-small-left" type="button">Save</button>
             </p>
           </div>
         </div>
