734f9b884173def4c098e567322627c7e0ca0448
Lincoln Anderson <landerson@352inc.com>
2019-02-19 15:06:17 -0500

    Abstract UK components. Shorthand utility classes.

--- src/App.js
+++ src/App.js
@@ -1,129 +1,56 @@
 import React, { Component } from 'react';
-import { compose, withPropsOnChange } from 'recompose';
-import cn from 'classnames';
 
-const withClassNames = (persistentClassNames) => withPropsOnChange(['className'], ({ className }) => ({
-  className: cn(persistentClassNames, className),
-}));
-
-const UK = {
-  primaryButton: compose(
-    withClassNames('uk-button uk-button-primary'),
-  )((props) => (
-    <button {...props} />
-  )),
-  button: compose(
-    withClassNames('uk-button uk-button-default'),
-  )((props) => (
-    <button {...props} />
-  )),
-  card: compose(
-    withClassNames('uk-card uk-card-default'),
-  )((props) => (
-    <div {...props} />
-  )),
-  cardBody: compose(
-    withClassNames('uk-card-body'),
-  )((props) => (
-    <div {...props} />
-  )),
-  stackedForm: compose(
-    withClassNames('uk-form-stacked'),
-  )((props) => (
-    <form {...props} />
-  )),
-  formControls: compose(
-    withClassNames('uk-form-controls'),
-  )((props) => (
-    <div {...props} />
-  )),
-  formLabel: compose(
-    withClassNames('uk-form-label'),
-  )((props) => (
-    <label {...props} />
-  )),
-  input: compose(
-    withClassNames('uk-input'),
-  )((props) => (
-    <input {...props} />
-  )),
-  modal: compose(
-  )((props) => (
-    <div data-uk-modal {...props} />
-  )),
-  modalDialog: compose(
-    withClassNames('uk-modal-dialog'),
-  )((props) => (
-    <div {...props} />
-  )),
-  modalBody: compose(
-    withClassNames('uk-modal-body'),
-  )((props) => (
-    <div {...props} />
-  )),
-};
-
-UK.primaryButton.defaultProps = {
-  type: 'button',
-};
-
-UK.button.defaultProps = {
-  type: 'button',
-};
-
-UK.input.defaultProps = {
-  type: 'text',
-};
+import UK from './components/UK';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <UK.button className="uk-margin-small-right" uk-toggle="target: #modal-example">Open</UK.button>
+        <UK.Button toggle="target: #modal-example">Open</UK.Button>
 
-        <UK.modal id="modal-example">
-          <UK.modalDialog>
-            <UK.modalBody>
-              <h2 className="uk-modal-title">Headline</h2>
+        <UK.Modal id="modal-example">
+          <UK.ModalDialog>
+            <UK.ModalBody>
+              <UK.ModalTitle>Headline</UK.ModalTitle>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-              <p className="uk-text-right">
-                <UK.button className="uk-modal-close">Cancel</UK.button>
-                <UK.primaryButton className="uk-margin-small-left">Save</UK.primaryButton>
+              <p className="text-right">
+                <UK.Button modal-close>Cancel</UK.Button>
+                <UK.PrimaryButton className="ml-3">Save</UK.PrimaryButton>
               </p>
-            </UK.modalBody>
-          </UK.modalDialog>
-        </UK.modal>
+            </UK.ModalBody>
+          </UK.ModalDialog>
+        </UK.Modal>
 
-        <div className="uk-background-muted uk-padding-large">
-          <UK.card className="uk-width-large uk-margin-auto-left uk-margin-auto-right">
-            <UK.cardBody>
+        <div className="bg-almost-white p-8">
+          <UK.Card className="w-lg mx-auto">
+            <UK.CardBody>
               <h2>Log in</h2>
 
-              <UK.stackedForm>
+              <UK.StackedForm>
 
-                <div className="uk-margin-bottom">
-                  <UK.formLabel htmlFor="login_username">Username</UK.formLabel>
-                  <UK.formControls>
-                    <UK.input id="login_username" />
-                  </UK.formControls>
+                <div className="mb-4">
+                  <UK.FormLabel htmlFor="login_username">Username</UK.FormLabel>
+                  <UK.FormControls>
+                    <UK.Input id="login_username" />
+                  </UK.FormControls>
                 </div>
 
-                <div className="uk-margin-bottom">
-                  <UK.formLabel htmlFor="login_password">Password</UK.formLabel>
-                  <UK.formControls>
-                    <UK.input id="login_password" type="password" />
-                  </UK.formControls>
+                <div className="mb-4">
+                  <UK.FormLabel htmlFor="login_password">Password</UK.FormLabel>
+                  <UK.FormControls>
+                    <UK.Input id="login_password" type="password" />
+                  </UK.FormControls>
                 </div>
 
-                <div className="uk-flex uk-flex-middle uk-flex-left uk-margin-large-top">
-                  <UK.primaryButton>Log in</UK.primaryButton>
-                  <a href="#" className="uk-text-muted uk-margin-left">Need to create an account?</a>
+                <div className="flex flex-middle flex-left mt-8">
+                  <UK.PrimaryButton>Log in</UK.PrimaryButton>
+                  <a href="#" className="text-muted ml-4">Need to create an account?</a>
                 </div>
 
-              </UK.stackedForm>
-              
-            </UK.cardBody>
-          </UK.card>
+              </UK.StackedForm>
+
+            </UK.CardBody>
+          </UK.Card>
         </div>
       </div>
     );
---
+++ src/components/UK.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import omit from 'lodash/omit';
+import { withPropsOnChange } from 'recompose';
+import cn from 'classnames';
+
+const withClassNames = (persistentClassNames) => withPropsOnChange(['className'], ({ className }) => ({
+  className: cn(persistentClassNames, className),
+}));
+
+const renderElement = (defaultTag) => (props) => React.createElement(props.tag || defaultTag, {
+  ...omit(props, ['modal-close', 'toggle', 'tag']),
+  className: cn(props.className, { 'uk-modal-close': props['modal-close'] }),
+  'data-uk-toggle': props.toggle,
+});
+
+const PrimaryButton = withClassNames('uk-button uk-button-primary')(renderElement('button'));
+PrimaryButton.defaultProps = {
+  type: 'button',
+};
+
+const Button = withClassNames('uk-button uk-button-default')(renderElement('button'));
+Button.defaultProps = {
+  type: 'button',
+};
+
+const Card = withClassNames('uk-card uk-card-default')(renderElement('div'));
+
+const CardBody = withClassNames('uk-card-body')(renderElement('div'));
+
+const StackedForm = withClassNames('uk-form-stacked')(renderElement('form'));
+
+const FormControls = withClassNames('uk-form-controls')(renderElement('div'));
+
+const FormLabel = withClassNames('uk-form-label')(renderElement('label'));
+
+const Input = withClassNames('uk-input')(renderElement('input'));
+Input.defaultProps = {
+  type: 'text',
+};
+
+const Modal = renderElement('div');
+Modal.defaultProps = {
+  'data-uk-modal': true,
+};
+
+const ModalDialog = withClassNames('uk-modal-dialog')(renderElement('div'));
+
+const ModalBody = withClassNames('uk-modal-body')(renderElement('div'));
+
+const ModalTitle = withClassNames('uk-modal-title')(renderElement('h2'));
+
+export default {
+  PrimaryButton,
+  Button,
+  Card,
+  CardBody,
+  StackedForm,
+  FormControls,
+  FormLabel,
+  Input,
+  Modal,
+  ModalDialog,
+  ModalBody,
+  ModalTitle,
+};
