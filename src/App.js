f877fc3bfccdf2804623fb353ac45f9c1a008846
Lincoln Anderson <landerson@352inc.com>
2019-02-22 18:17:07 -0500

    Add tailwind utilities (NOT optimized)

--- src/App.js
+++ src/App.js
@@ -13,38 +13,38 @@ class App extends Component {
             <UK.ModalBody>
               <UK.ModalTitle>Headline</UK.ModalTitle>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-              <p className="uk-text-right">
+              <p className="text-right">
                 <UK.Button modal-close>Cancel</UK.Button>
-                <UK.PrimaryButton className="uk-margin-small-left">Save</UK.PrimaryButton>
+                <UK.PrimaryButton className="ml-3">Save</UK.PrimaryButton>
               </p>
             </UK.ModalBody>
           </UK.ModalDialog>
         </UK.Modal>
 
-        <div className="uk-background-muted uk-padding-large">
-          <UK.Card className="uk-width-large uk-margin-auto-left uk-margin-auto-right">
+        <div className="bg-grey-lighter p-8">
+          <UK.Card className="max-w-sm mx-auto">
             <UK.CardBody>
               <h2>Log in</h2>
 
               <UK.StackedForm>
 
-                <div className="uk-margin-medium-bottom">
+                <div className="mb-4">
                   <UK.FormLabel htmlFor="login_username">Username</UK.FormLabel>
                   <UK.FormControls>
                     <UK.Input id="login_username" />
                   </UK.FormControls>
                 </div>
 
-                <div className="uk-margin-bottom">
+                <div className="mb-4">
                   <UK.FormLabel htmlFor="login_password">Password</UK.FormLabel>
                   <UK.FormControls>
                     <UK.Input id="login_password" type="password" />
                   </UK.FormControls>
                 </div>
 
-                <div className="uk-flex uk-flex-middle uk-flex-left uk-margin-large-top">
+                <div className="flex items-center justify-start mt-8">
                   <UK.PrimaryButton>Log in</UK.PrimaryButton>
-                  <a href="#" className="uk-text-muted uk-margin-left">Need to create an account?</a>
+                  <a href="#" className="text-grey ml-4">Need to create an account?</a>
                 </div>
 
               </UK.StackedForm>
