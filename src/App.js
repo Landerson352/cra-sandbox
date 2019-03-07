c4e71131f3a7f71a33667b4ae2cf4737fecea541
Lincoln Anderson <landerson@352inc.com>
2019-03-07 11:16:47 -0500

    Add apollo with local state.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
+import { ApolloProvider, Query } from 'react-apollo';
+import ApolloClient from 'apollo-boost';
+import gql from 'graphql-tag';
+
+const activeState = {
+  active: false,
+};
+
+const getActiveQuery = gql`
+  {
+    active @client
+  }
+`;
+
+const toggleActive = ({ data, client }) => {
+  client.writeData({ data: { active: !data.active } });
+};
+
+const client = new ApolloClient({
+  clientState: {
+    defaults: {
+      ...activeState,
+    },
+  },
+});
 
 class App extends Component {
   render() {
     return (
-      <Fragment>
-          Welcome to React
-      </Fragment>
+      <ApolloProvider client={client}>
+        <Query query={getActiveQuery}>
+          {({ data, client }) => (
+            <button
+              type="button"
+              onClick={() => toggleActive({ data, client })}
+            >
+              {data.active ? 'active' : 'inactive'}
+            </button>
+          )}
+        </Query>
+      </ApolloProvider>
     );
   }
 }
